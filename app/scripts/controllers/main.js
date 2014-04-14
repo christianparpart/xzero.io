831adff3fa6814b2b85ab0e506d8e414f880b6fa
Christian Parpart <trapni@gmail.com>
2014-04-14 11:12:36 +0200

    updates

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,17 @@
 'use strict';
 
+//angular.module('webappAngularApp', ['angular-google-analytics'])
 angular.module('webappAngularApp')
-  .controller('MainCtrl', function ($scope) {
+
+//  .config(function('AnalyticsProvider') {
+//    AnalyticsProvider.setAccount('UA-49288045-1');
+//    AnalyticsProvider.trackPages(true);
+//    AnalyticsProvider.useAnalytics(false); // analytics.js instead of ga.js ?
+//    AnalyticsProvider.useEnhancedLinkAttribution(true);
+//    AnalyticsProvider.setPageEvent('$stateChangeSuccess');
+//  })
+
+  .controller('MainCtrl', function ($scope, $location, $window) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
